b3a053842bf4efff72793ec3ed195a44e030a0d5
ksraj123 <39027928+ksraj123@users.noreply.github.com>
2019-03-31 10:26:57 -0700

    Update Raw12Img.h

--- src/headers/Raw12Img.h
+++ src/headers/Raw12Img.h
@@ -9,36 +9,36 @@
 #define totalPix (4096 * 3072)
 
 struct streams
-{
-    std::ifstream intputfile;
-    std::ofstream output_red, output_blue, output_green;
-    std::ofstream file_output_ppm;
-};
+ {
+      std::ifstream intputfile;
+      std::ofstream output_red, output_blue, output_green;
+      std::ofstream file_output_ppm;
+ };
 
 struct channels
-{
-    std::vector <unsigned char> red;
-    std::vector <unsigned char> blue;
-    std::vector <unsigned char> green;
+ {
+      std::vector <unsigned char> red;
+      std::vector <unsigned char> blue;
+      std::vector <unsigned char> green;
 
-    void Push(unsigned char r, unsigned char g, unsigned char b)
-    {
-        red.push_back(r);
-        green.push_back(g);
-        blue.push_back(b);
-    }
-};
+      void Push(unsigned char r, unsigned char g, unsigned char b)
+      {
+           red.push_back(r);
+           green.push_back(g);
+           blue.push_back(b);
+      }
+ };
 
 class Raw12Img
-{
-    std::string file_name;
+ {
+      std::string file_name;
 
-    public:
-        Raw12Img (std::string);
-        void Load();
-        void Debayer_channels();
-        void Write_channels();
-        void Write_debayered_image();
-};
+      public:
+          Raw12Img (std::string);
+          void Load();
+          void Debayer_channels();
+          void Write_channels();
+          void Write_debayered_image();
+ };
 
 #endif
