92599905d1198b0086b25495b439354e32c3f45b
ksraj123 <39027928+ksraj123@users.noreply.github.com>
2019-03-31 08:34:27 -0700

    Update functional.h

--- src/headers/functional.h
+++ src/headers/functional.h
@@ -5,9 +5,9 @@
 #include <cstdint>
 #include <cstdlib>
 #include <string>
-
-void execution();
+#include <vector>
 
 #define max_width 4096
 #define max_height 3072
-
+#define inputSize (4096 * 3072 * 3 / 2)
+#define totalPix (4096 * 3072)
