a5bd724ac913a641857a63f5bad7572a324be089
ksraj123 <39027928+ksraj123@users.noreply.github.com>
2019-03-31 08:32:04 -0700

    Update Demosaicing.h

--- src/headers/Demosaicing.h
+++ src/headers/Demosaicing.h
@@ -1,9 +1,8 @@
 #include "functional.h"
 
-namespace Demosaic{
-
-void green(uint8_t c_g[max_height+1][max_width+1]);
-void blue(uint8_t c_b[max_height+1][max_width+1]);
-void red(uint8_t c_r[max_height+1][max_width+1]);
-
+namespace Demosaic
+{
+void Green(unsigned char* arr);
+void Blue(unsigned char* arr);
+void Red(unsigned char* arr);
 }
