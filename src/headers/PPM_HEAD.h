a5e253f1fd5b3917e1dbe6a5f5c8854c82020cf6
ksraj123 <39027928+ksraj123@users.noreply.github.com>
2019-03-31 10:24:58 -0700

    Update PPM_HEAD.h

--- src/headers/PPM_HEAD.h
+++ src/headers/PPM_HEAD.h
@@ -3,10 +3,10 @@
 #include "functional.h"
 
 namespace OutputImage
-{
+ {
 namespace pixelmap
-{
-    void Write_channel(std::ofstream*, unsigned char* arr, std::string);
-}
-}
+ {
+      void Write_channel(std::ofstream*, unsigned char* arr, std::string);
+ }
+ }
 #endif
