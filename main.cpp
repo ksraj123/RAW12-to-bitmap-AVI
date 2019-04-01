39694e88a12d627eefbcaf5680c174572fbfbbf6
ksraj123 <39027928+ksraj123@users.noreply.github.com>
2019-04-01 04:17:40 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include "src/headers/functional.h"
 #include "src/headers/Raw12Img.h"
 
-int main(){
-std::cout << "//////// !!!! KUMAR SAURABH RAJ !!!!///////\n";
-
-Raw12Img InputImg("test_image/test_image.raw12");
-
-std::cout << "Loading Raw12Image\n";
-InputImg.Load();
-std::cout << "Debayering channels\n";
-InputImg.DebayerChannels();
-std::cout << "Writing chaneels to results/ \n";
-InputImg.WriteChannels();
-std::cout << "Writing debayered BMP Image to results/ \n";
-InputImg.WriteDebayered();
-std::cout << "Please check results folder after sometime for output images\n";
-
-std::cout << "!!!!Thank you for using my program!!!!";
+int main()
+{
+    Raw12Img InputImg("test_image/test_image.raw12");
+    std::cout << "//////// !!!! KUMAR SAURABH RAJ !!!!///////\n";
+    std::cout << "Loading Raw12Image\n";
+    InputImg.Load();
+    std::cout << "Debayering channels\n";
+    InputImg.DebayerChannels();
+    std::cout << "Writing chaneels to results/ \n";
+    InputImg.WriteChannels();
+    std::cout << "Writing debayered BMP Image to results/ \n";
+    InputImg.WriteDebayered();
+    std::cout << "Please check results folder after sometime for output images\n";
+    std::cout << "!!!!Thank you for using my program!!!!";
 }
