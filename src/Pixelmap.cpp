340c826487f5bc2944cbc7bef4212068402dde1e
ksraj123 <39027928+ksraj123@users.noreply.github.com>
2019-04-02 12:16:06 -0700

    Update Pixelmap.cpp

--- src/Pixelmap.cpp
+++ src/Pixelmap.cpp
@@ -2,36 +2,37 @@
 
 namespace OutputImage::PixelMap
 {
-    void Write(std::ofstream* channel, unsigned char* arr, std::string chanel_name)
+    void Write(unsigned char* arr, std::string chanel_name)
     {
         std::string output_file_name = "result/" + chanel_name + ".ppm";
-        channel->open(output_file_name.c_str(), std::ios::out | std::ios::trunc);
+        std::ofstream channel;
+        channel.open(output_file_name.c_str(), std::ios::out | std::ios::trunc);
         if(!channel)
         {
             std::cerr << "Error: File cannot be opened\n\tExiting\n";
             exit(1);
         }
-        *channel << "P3\n" << "# " << chanel_name << ".ppm\n" << max_width
+        channel << "P3\n" << "# " << chanel_name << ".ppm\n" << max_width
                  << " " << max_height << "\n" << "255" << "\n";
         for (int itr = 0; itr < totalPix; itr++)
         {
             switch(chanel_name[0])
             {
                 case 'r':
-                    *channel << (int)arr[itr] << " 0 0";
+                    channel << (int)arr[itr] << " 0 0";
                     break;
                 case 'g':
-                    *channel << "0 " << (int)arr[itr] << " 0";
+                    channel << "0 " << (int)arr[itr] << " 0";
                     break;
                 case 'b':
-                    *channel << "0 0 " << (int)arr[itr];
+                    channel << "0 0 " << (int)arr[itr];
                     break;
             }
             if ((itr + 1) % max_width == 0)
-                *channel << "\n";
+                channel << "\n";
             else
-                *channel << " ";
+                channel << " ";
         }
-        channel->close();
+        channel.close();
     }
 }
