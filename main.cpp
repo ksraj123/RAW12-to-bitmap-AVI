9faafd8bb6cd2b1d2b8fa1a2178267e2650d3c73
ksraj123 <39027928+ksraj123@users.noreply.github.com>
2019-04-02 18:55:27 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,20 @@
 int main()
 {
     Raw12Img InputImg("test_image/test_image.raw12");
+
     std::cout << "//////// !!!! KUMAR SAURABH RAJ !!!!///////\n";
     std::cout << "Loading Raw12Image\n";
     InputImg.Load();
+
     std::cout << "Debayering channels\n";
     InputImg.DebayerChannels();
+
     std::cout << "Writing chaneels to results/ \n";
     InputImg.WriteChannels();
+
     std::cout << "Writing debayered BMP Image to results/ \n";
     InputImg.WriteDebayered();
+
     std::cout << "Please check results folder after sometime for output images\n";
     std::cout << "!!!!Thank you for using my program!!!!";
 }
