9350b4dac7b4d294f70192e22365b4df7afd79d6
ksraj123 <39027928+ksraj123@users.noreply.github.com>
2019-04-03 00:04:57 -0700

    Update Raw12Img.h

--- src/headers/Raw12Img.h
+++ src/headers/Raw12Img.h
@@ -2,27 +2,10 @@
 #define Raw12Img_H
 
 #include "functional.h"
-#include "BMP_HEAD.h"
-#include "PPM_HEAD.h"
+#include "OutImg.h"
 #include "Demosaicing.h"
 #define inputSize (4096 * 3072 * 3 / 2)
 #define totalPix (4096 * 3072)
-
-struct Channels
-{
-    uint8_t red[totalPix]{0};
-    uint8_t blue[totalPix]{0};
-    uint8_t green[totalPix]{0};
-
-    void Push(uint8_t r, uint8_t g, uint8_t b)
-    {
-        static int index = 0;
-        red[index] = r;
-        green[index] = g;
-        blue[index] = b;
-        index++;
-    }
-};
 
 class Raw12Img
 {
@@ -34,6 +17,32 @@ class Raw12Img
     void DebayerChannels();
     void WriteChannels();
     void WriteDebayered();
+};
+
+class Channels
+{
+    public:
+    uint8_t *red;
+    uint8_t *blue;
+    uint8_t *green;
+    char* fileData;
+
+    Channels()
+    {
+        red = new uint8_t[totalPix]{0};
+        blue = new uint8_t[totalPix]{0};
+        green = new uint8_t[totalPix]{0};
+        fileData = new char [inputSize]{0};
+    }
+
+    void Push(uint8_t r, uint8_t g, uint8_t b)
+    {
+        static int index = 0;
+        red[index] = r;
+        green[index] = g;
+        blue[index] = b;
+        index++;
+    }
 };
 
 #endif
