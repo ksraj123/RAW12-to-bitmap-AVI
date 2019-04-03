77c02995a58c4ef2f7bc5a0e9399f6d5ca2f9fb6
ksraj123 <39027928+ksraj123@users.noreply.github.com>
2019-04-03 08:47:19 -0700

    Update Raw12Img.cpp

--- src/Raw12Img.cpp
+++ src/Raw12Img.cpp
@@ -2,21 +2,41 @@
 #define Sensel(A, B) ((A & 0x0F) << 4 | (B & 0xF0) >> 4)
 #include <endian.h>
 
-Raw12Img::Raw12Img(std::string t_fileName)
-            : m_fileName(t_fileName)
+// constructor of class Channels
+Channels::Channels()
 {
+    red = new uint8_t[2*totalPix]{0};
+    blue = new uint8_t[2*totalPix]{0};
+    green = new uint8_t[2*totalPix]{0};
+    fileData = new char [2*inputSize]{0};
 }
 
-void Raw12Img::Load()
+// member fuction of class Channels
+void Channels::Push(uint8_t r, uint8_t g, uint8_t b)
 {
-    std::ifstream intputFile;
-    intputFile.open(m_fileName.c_str(), std::ios::binary);
-    if(!intputFile)
+    static int index = 0;
+    red[index] = r;
+    green[index] = g;
+    blue[index] = b;
+    index++;
+}
+
+// constructor of class Raw12Img
+Raw12Img::Raw12Img(std::string filePath)
+            : _filePath(filePath)
+{
+    _intputFile.open(_filePath, std::ios::binary);
+    if(!_intputFile)
     {
         std::cerr << "Error: File cannot be opened\b\tExiting" << std::endl;
         exit(1);
     }
-    intputFile.read(chnl.fileData, inputSize);
+}
+
+// member fuction of class Raw12Img
+void Raw12Img::Load()
+{
+    _intputFile.read(chnl.fileData, inputSize);
     for (int itr = 0; itr < inputSize; itr+= 3)
     {
         int row = (itr * 2) / (max_width * 3);
@@ -31,9 +51,10 @@ void Raw12Img::Load()
             chnl.Push(0, 0, Sensel(chnl.fileData[itr+1], chnl.fileData[itr+2]));
         }
     }
-    intputFile.close();
+    _intputFile.close();
 }
 
+// member fuction of class Raw12Img
 void Raw12Img::DebayerChannels()
 {
     // Calling methods in namespace Demosaic
@@ -42,14 +63,24 @@ void Raw12Img::DebayerChannels()
     Demosaic::Type1(chnl.blue, 0);
 }
 
+// member fuction of class Raw12Img
 void Raw12Img::WriteChannels()
 {
+    // WritePpm() declared in headers/OutImg.h
     WritePpm(chnl.red, "red");
     WritePpm(chnl.green, "green");
     WritePpm(chnl.blue, "blue");
 }
 
+// member fuction of class Raw12Img
 void Raw12Img::WriteDebayered()
 {
+    // WriteBmp() declared in headers/OutImg.h
     WriteBmp(chnl.red, chnl.blue, chnl.green);
+}
+
+
+void Raw12Img::WriteAVI()
+{
+    WriteToAvi(chnl.red, chnl.blue, chnl.green);
 }
