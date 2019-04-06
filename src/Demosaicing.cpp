b1c841dba9e3985ea6b9dd51c69b3aec7ac5638a
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2019-04-05 21:24:33 -0700

    formatting adjustments

--- src/Demosaicing.cpp
+++ src/Demosaicing.cpp
@@ -18,20 +18,20 @@ void Demosaic::DemosaicChannels(InputImage* input)
 void Demosaic::RedBlue(uint8_t* channel, int startPos)
 {
     int row;
-    for (int itrChnl = startPos; itrChnl < TOTAL_PIX; itrChnl+=2)
+    for (int itrChnl = startPos; itrChnl < TOTAL_PIX; itrChnl += 2)
     {
         row = itrChnl / IMAGE_WIDTH;
         if (row % 2 == startPos)
         {
-            channel[itrChnl-1] = (channel[itrChnl-1-IMAGE_WIDTH] 
-                                + channel[itrChnl-1+IMAGE_WIDTH])/2;
-            channel[itrChnl+1] = (channel[itrChnl+1-IMAGE_WIDTH] 
-                                + channel[itrChnl+1+IMAGE_WIDTH])/2;
+            channel[itrChnl - 1] = (channel[itrChnl - 1 - IMAGE_WIDTH] 
+                                 + channel[itrChnl - 1 + IMAGE_WIDTH]) / 2;
+            channel[itrChnl + 1] = (channel[itrChnl+1-IMAGE_WIDTH] 
+                                 + channel[itrChnl+1+IMAGE_WIDTH]) / 2;
         }
-        channel[itrChnl] = (channel[itrChnl-1] + channel[itrChnl+1])/2;
+        channel[itrChnl] = (channel[itrChnl - 1] + channel[itrChnl + 1]) / 2;
     }
     std::fill(channel, channel+IMAGE_WIDTH, 0); // first row
-    std::fill(channel+TOTAL_PIX-IMAGE_WIDTH, channel+TOTAL_PIX, 0); //last row
+    std::fill(channel + TOTAL_PIX - IMAGE_WIDTH, channel + TOTAL_PIX, 0); //last row
  }
 
 // This fuctions demosaics green channel
@@ -39,16 +39,16 @@ void Demosaic::Green(uint8_t* channel)
 {
     for (int itrChnl = 0; itrChnl < TOTAL_PIX;)
     {
-        channel[itrChnl] = (channel[itrChnl-1] + channel[itrChnl+1] 
-                          + channel[itrChnl-IMAGE_WIDTH]
-                          + channel[itrChnl+IMAGE_WIDTH])/4;
-        if (itrChnl/IMAGE_WIDTH != (itrChnl+2)/IMAGE_WIDTH)
+        channel[itrChnl] = (channel[itrChnl - 1] + channel[itrChnl + 1] 
+                          + channel[itrChnl - IMAGE_WIDTH]
+                          + channel[itrChnl + IMAGE_WIDTH])/4;
+        if (itrChnl / IMAGE_WIDTH != (itrChnl + 2) / IMAGE_WIDTH)
         {
-            itrChnl+= 3;
+            itrChnl += 3;
             continue;
         }
-        itrChnl+=2;
+        itrChnl += 2;
     }
     std::fill(channel, channel+IMAGE_WIDTH, 0); //first row
-    std::fill(channel+TOTAL_PIX-IMAGE_WIDTH, channel+TOTAL_PIX, 0); //last row
+    std::fill(channel + TOTAL_PIX - IMAGE_WIDTH, channel + TOTAL_PIX, 0); //last row
 }
--- src/Output.cpp
+++ src/Output.cpp
@@ -38,7 +38,7 @@ void OutputImage::WritePpm(std::string chanelName)
                 break;
         }
     // Entire Image written at once for enhanced performace
-    outputFile.write(reinterpret_cast<const char*> (ppmBin), TOTAL_PIX*3);
+    outputFile.write(reinterpret_cast<const char*> (ppmBin), TOTAL_PIX * 3);
     delete ppmBin;
     outputFile.close();
 }
@@ -59,7 +59,7 @@ void OutputImage::WriteBmp()
     outputFile.write(reinterpret_cast<const char*>(&flleHeader), sizeof(flleHeader));
     outputFile.write(reinterpret_cast<const char*>(&infoHeader), sizeof(infoHeader));
     // Entire Image written at once for enhanced performace
-    outputFile.write(reinterpret_cast<const char*>(bmpImage), 3*TOTAL_PIX);
+    outputFile.write(reinterpret_cast<const char*>(bmpImage), 3 * TOTAL_PIX);
     outputFile.close();
     if (!outputFile)
     {
@@ -78,11 +78,11 @@ void OutputImage::WriteToAvi()
         exit(1);
     }
     uint32_t size_pix = TOTAL_PIX * 3;
-    uint32_t size_rec = 3*4 + size_pix;
-    uint32_t size_movi = 3*4 + size_rec;
-    uint32_t size_strl = 3*4 + sizeof(streamHeader) + 2*4 + sizeof(streamFormat);
-    uint32_t size_hrdl =  3*4 + sizeof(mainHeader) + 2*4 + size_strl;
-    uint32_t size_riff = 3 + 2*4 + size_hrdl + 2*4 + size_movi;
+    uint32_t size_rec = 3 * 4 + size_pix;
+    uint32_t size_movi = 3 * 4 + size_rec;
+    uint32_t size_strl = 3 * 4 + sizeof(streamHeader) + 2 * 4 + sizeof(streamFormat);
+    uint32_t size_hrdl =  3 * 4 + sizeof(mainHeader) + 2 * 4 + size_strl;
+    uint32_t size_riff = 3 + 2 * 4 + size_hrdl + 2 * 4 + size_movi;
 
     outputFile.write("RIFF", 4);
     outputFile.write(reinterpret_cast<const char*>(&size_riff), 4);
@@ -137,9 +137,9 @@ void OutputImage::WriteToAvi()
 // Returns pointer to image to be written in P6 PPM
 uint8_t* OutputImage::GetPpmChannel(uint8_t* channel, std::string chanelName)
 {
-    uint8_t* ppmBin = new uint8_t[3*TOTAL_PIX]{0};
+    uint8_t* ppmBin = new uint8_t[3 * TOTAL_PIX]{0};
 
-    for (int itrPpmBin = 0, itrChnl = 0; itrPpmBin < 3*TOTAL_PIX, 
+    for (int itrPpmBin = 0, itrChnl = 0; itrPpmBin < 3 * TOTAL_PIX, 
                             itrChnl < TOTAL_PIX; itrPpmBin += 3, ++itrChnl)
     {
         switch (chanelName[0])
@@ -148,10 +148,10 @@ uint8_t* OutputImage::GetPpmChannel(uint8_t* channel, std::string chanelName)
                 ppmBin[itrPpmBin] = channel[itrChnl];
                 break;
             case 'g':
-                ppmBin[itrPpmBin+1] = channel[itrChnl];
+                ppmBin[itrPpmBin + 1] = channel[itrChnl];
                 break;
             case 'b':
-                ppmBin[itrPpmBin+2] = channel[itrChnl];
+                ppmBin[itrPpmBin + 2] = channel[itrChnl];
                 break;
         }      
     }
@@ -162,15 +162,15 @@ uint8_t* OutputImage::GetPpmChannel(uint8_t* channel, std::string chanelName)
 uint8_t* OutputImage::GetBmpImage(uint8_t* channelRed, uint8_t* channelBlue, 
                                   uint8_t* channelGreen)
 {
-    uint8_t* bmpBin = new uint8_t [TOTAL_PIX*3];
-    for (int row = IMAGE_HEIGHT-1, itr = 0; row >= 0; --row)
+    uint8_t* bmpBin = new uint8_t [TOTAL_PIX * 3];
+    for (int row = IMAGE_HEIGHT - 1, itr = 0; row >= 0; --row)
     {
-        for (int col = row * IMAGE_WIDTH; col < (row+1)*IMAGE_WIDTH; ++col)
+        for (int col = row * IMAGE_WIDTH; col < (row + 1) * IMAGE_WIDTH; ++col)
         {
             bmpBin[itr] = channelBlue[col];
-            bmpBin[itr+1] = channelGreen[col];
-            bmpBin[itr+2] = channelRed[col];
-            itr+=3;
+            bmpBin[itr + 1] = channelGreen[col];
+            bmpBin[itr + 2] = channelRed[col];
+            itr += 3;
         }
     }
     return bmpBin;
